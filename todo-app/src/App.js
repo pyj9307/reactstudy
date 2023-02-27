ebc485feb55a69a612f45593cfb8fccc873b059f
pyjreact <iy1566@naver.com>
2023-02-27 13:51:31 +0900

    4교시 5장 일정관리 프로그램

---
+++ todo-app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import TodoTemplate from './components/TodoTemplate';
+
+function App() {
+  return (
+    <TodoTemplate>Todo App을 만들자.</TodoTemplate>
+  );
+}
+
+export default App;
